2d6a9b8db8456e24e1d9477d465857d3c12090ff
Christian Gosselin <christian.gosselin@bnc.ca>
2020-02-11 13:37:45 -0500

    google terraform cred path

--- zshrc.sh
+++ zshrc.sh
@@ -4,6 +4,9 @@ export PATH=$HOME/bin:$HOME/Tools/google-cloud-sdk/bin:$PATH
 # Path to your oh-my-zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
 
+# Path to credential for Terraform to use GCP
+export GOOGLE_APPLICATION_CREDENTIALS="$HOME/credential.json"
+
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
