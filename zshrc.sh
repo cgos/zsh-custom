5c16228e566ba078e27f0d7334399f3bffcc471a
Christian Gosselin <cgossel@amazon.com>
2021-01-02 14:05:10 -0500

    modify less behavior

--- zshrc.sh
+++ zshrc.sh
@@ -4,6 +4,8 @@ export PATH=$HOME/bin:$PATH
 # Path to your oh-my-zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
 
+# To prevent less from clearing the screen on exit 
+export LESS="-X"
 
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time oh-my-zsh is loaded, in which case,
